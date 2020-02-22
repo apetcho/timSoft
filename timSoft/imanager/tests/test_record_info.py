57f9cb0031eacda2a8fd231f6fe025a2b967f148
apetcho <eyram.apetcho@gmail.com>
2020-02-22 13:06:36 +0100

    Tested timestampinfo() successfull

--- timSoft/imanager/tests/test_record_info.py
+++ timSoft/imanager/tests/test_record_info.py
@@ -4,3 +4,47 @@
 Created on Sat Feb 22, 2020 11:26:22 AM
 @author: eapetcho
 """
+import unittest
+from timSoft.imanager.record_info import RecordInfo
+
+# Input data for testing purpose
+description = "Bus ticket"
+value = 60.12
+recObj = RecordInfo(description, value)
+now = recObj.timestamp
+
+
+class TestRecordInfo(unittest.TestCase):
+
+    def test_timestampinfo(self):
+        """Test timestampinfo() method"""
+        dateinfo, timeinfo = now.split('T')
+        month, day, year = dateinfo.split('-')
+        hour, minute, second = timeinfo.split(':')
+        info = {'month': month, "day": day, "year": year,
+                "hour": hour, "minute": minute, "second": second}
+        print("Testing timestampinfo() method")
+        self.assertEqual(RecordInfo.timestampinfo(recObj), info)
+        print("Test finished")
+
+    def test_set_tag(self):
+        pass
+
+    def test_set_earning(self):
+        pass
+
+    def test_get_value(self):
+        pass
+
+    def test_get_description(self):
+        pass
+
+    def test_get_timestamp(self):
+        pass
+
+    def test_get_record_line(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
